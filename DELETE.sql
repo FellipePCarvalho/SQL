55f855086d3b548701779ccb9d6cebdda2f99660
Fellpires <fellipepires7@gmail.com>
2024-09-18 10:20:05 -0300

    comando delete

---
+++ DELETE.sql
@@ -0,0 +1,11 @@
+-- COMANDO DELETE
+
+-- Selecionando a tabela Aula
+SELECT * FROM aula
+
+-- Deletando uma linha
+DELETE FROM aula
+WHERE nome = 'mudei'
+
+-- Deletando todas as linhas
+DELETE FROM aula
