75103f9a94ef38948bd110b57a40035c28beb92d
Fellpires <fellipepires7@gmail.com>
2024-09-18 10:24:34 -0300

    comando select

---
+++ SELECT.sql
@@ -0,0 +1,17 @@
+-- Comando Select
+
+-- Primeiros testes
+SELECT *
+FROM person.Person;
+
+SELECT Title
+FROM person.Person;
+
+SELECT *
+FROM person.EmailAddress;
+
+-- Desafios 
+-- 1. A equipe de marketing precisa fazer uma pesquisa sobre nomes mais comuns de seus clientes e precisa do nome e sobrenome de todos os clientes que estão cadastrados no sistema.
+
+SELECT firstName, lastName
+FROM person.Person;
