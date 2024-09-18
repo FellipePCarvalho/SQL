95da4f17c0f3ea1c3f380b5f70a85b451d964ba9
Fellpires <fellipepires7@gmail.com>
2024-09-18 10:25:17 -0300

    comando distinct

---
+++ DISTINCT.sql
@@ -0,0 +1,13 @@
+-- Comando DISCTINCT
+
+-- Primeiros testes
+SELECT DISTINCT FirstName
+FROM person.Person;
+
+-- Desafios
+-- Quantos sobrenomes únicos temos em nossa tabela person.person?
+
+SELECT DISTINCT LastName
+FROM person.Person;
+
+-- 1.206 nomes
